b486e11dfbac0dd8ca598bb19fefc583e0e9d41f
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-01-17 12:51:51 -0800

    Move images folder to src and add Hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Hero from './components/Hero';
 
 class App extends Component {
   render() {
-    return <Nav />;
+    return (
+      <div>
+        <Nav />
+        <Hero />
+      </div>
+    );
   }
 }
 
