6f38ddd1c0d658ab099fe279ca6e3cf6276a1a7c
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-01-18 20:27:33 -0800

    Add About component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Work from './components/Work';
+import About from './components/About';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Nav />
         <Hero />
         <Work />
+        <About />
       </div>
     );
   }
