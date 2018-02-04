8fc31483f07629babb02e5bffeb67c525616e5af
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-02-03 19:26:21 -0800

    Bug Fix - scroll top on route change to ProjectDetail pages

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -4,41 +4,49 @@ import { Link } from 'react-router-dom';
 import Nav from './Nav';
 import Footer from './Footer';
 
-const ProjectDetails = props => {
-  const project = props.project;
-  return (
-    <div>
-      <Nav />
+class ProjectDetails extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-      <div className="project-detail">
-        <div className="container project-detail__inner">
-          <p>
-            <Link className="link--back" to="/">Back</Link>
-          </p>
-          <h1>{project.title}</h1>
-          <div className="project-detail__content">
-            <div className="project-detail__image">
-              <img
-                className="shadow"
-                src={`${process.env.PUBLIC_URL}/img/${project.image}`}
-                alt={project.title}
-              />
-            </div>
-            <div className="project-detail__text">
-              <h3>Summary</h3>
-              <p>{project.description}</p>
-              <h3>Tech Used</h3>
-              <ul>
-                {project.technologies.map(tech => <li key={tech}>{tech}</li>)}
-              </ul>
+  render() {
+    const project = this.props.project;
+    return (
+      <div>
+        <Nav />
+
+        <div className="project-detail">
+          <div className="container project-detail__inner">
+            <p>
+              <Link className="link--back" to="/">
+                Back
+              </Link>
+            </p>
+            <h1>{project.title}</h1>
+            <div className="project-detail__content">
+              <div className="project-detail__image">
+                <img
+                  className="shadow"
+                  src={`${process.env.PUBLIC_URL}/img/${project.image}`}
+                  alt={project.title}
+                />
+              </div>
+              <div className="project-detail__text">
+                <h3>Summary</h3>
+                <p>{project.description}</p>
+                <h3>Tech Used</h3>
+                <ul>
+                  {project.technologies.map(tech => <li key={tech}>{tech}</li>)}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <Footer />
-    </div>
-  );
-};
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default ProjectDetails;
